9bf8b33b164317efa387930f8ae5cf8735f09845
Rohan <42150117+rohan2398@users.noreply.github.com>
2020-04-14 02:15:42 -0700

    Delete TodoUpdateModel.cs

--- TodoUpdateModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GIBDemo.Triggers.Functions
-{
-    internal class TodoUpdateModel
-    {
-        public object TaskDescription { get; internal set; }
-        public object IsCompleted { get; internal set; }
-    }
-}
