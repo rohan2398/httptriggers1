af77764706d117d1a7d87be8390c07f5f9fbbf23
Rohan <42150117+rohan2398@users.noreply.github.com>
2020-05-19 01:33:10 -0700

    Add files via upload

---
+++ testFunction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Linq;
+using System.Net.Http;
+using System.Linq;
+using Microsoft.Azure.Management.CosmosDB.Fluent;
+using System.Configuration;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.WebJobs.Host;
+using Castle.Core.Internal;
+using EmployeeFunction;
+using ECP.Master.tests;
+
+namespace FunctionApp2.TESTING
+{
+   public class testFunction
+    {
+        private Mock<ILogger> loggerMock;
+
+        [Fact]
+        public void TestSetup()
+        {
+            this.loggerMock = new Mock<ILogger>();
+        }
+
+            [Fact]
+        public void CreateEmployeeList()
+        {
+            this.TestSetup();
+
+            EmployeeEntity input = new EmployeeEntity();
+            input.employeeId = "TestEmployeeId";
+
+            var req = new HttpRequestMessage();
+            req.Content = new StringContent(JsonConvert.SerializeObject(input), Encoding.UTF8, "application/json");
+            req = TestHelpers.SetupHttp(req);
+
+
+        }
+
+    }
+}
